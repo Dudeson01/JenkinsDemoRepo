5f635b01c654e500ece261bab51d56be99a75504
mithu <mithu@MyDell>
2025-04-06 12:12:59 +0530

    poll SCM class added

---
+++ jenkins/src/test/java/demo/PollSCMTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void pollSCMTest() {
+		Reporter.log("POll scm test added",true);
+	}
+
+}
